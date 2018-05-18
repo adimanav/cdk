1e9087ccebb6440b3d2ae035d7c96589492c99e5
Aditya Iyer <aditya_iyer@yahoo.com>
2018-05-18 09:18:19 +0530

    first

---
+++ substitute/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using substitute;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [DeploymentItem(@"testfiles\inputfile1.txt")]
+        [DeploymentItem(@"testfiles\inputdata1.txt")]
+        [DeploymentItem(@"testfiles\outputfile1.txt")]
+        public void TestMethod1()
+        {
+            string expectedOutputFile = "outputfile1.txt";
+            string actualOutputFile = Guid.NewGuid() + ".txt";
+            Substitute.Process(
+                new[] {
+                    "inputfile1.txt",
+                    "inputdata1.txt",
+                    actualOutputFile
+                });
+
+            var expected = File.ReadAllBytes(expectedOutputFile);
+            var actual = File.ReadAllBytes(actualOutputFile);
+
+            Assert.AreEqual<int>(expected.Length, actual.Length);
+            for (int i = 0; i < expected.Length; i++)
+                Assert.AreEqual<byte>(expected[i], actual[i]);
+        }
+
+        private byte[] GetMD5Hash(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    return md5.ComputeHash(stream);
+                }
+            }
+        }
+    }
+}
---
+++ substitute/substitute/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace substitute
+{
+    public class Substitute
+    {
+        public static void Process(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Parmeters input text file, input data file and output file missing!");
+                return;
+            }
+
+            using (TextReader textReader = File.OpenText(args[0]))
+            {
+                using (TextReader dataReader = File.OpenText(args[1]))
+                {
+                    using (StreamWriter writer = new StreamWriter(args[2]))
+                    {
+                        var dataMap = new Dictionary<string, string>();
+                        string data = dataReader.ReadToEnd();
+                        var dataArr = data.Split('\n');
+                        foreach (var dataEl in dataArr)
+                        {
+                            if (!string.IsNullOrWhiteSpace(dataEl))
+                            {
+                                var pair = dataEl.Split('=');
+                                dataMap[pair[0].Trim().ToLower()] = pair[1].Trim();
+                            }
+                        }
+
+                        string line = null;
+                        bool started = false;
+                        StringBuilder tmp = new StringBuilder();
+
+                        while (null != (line = textReader.ReadLine()))
+                        {
+                            StringBuilder newLine = new StringBuilder();
+                            foreach (var ch in line)
+                            {
+                                if (started)
+                                {
+                                    if (ch != '%')
+                                        tmp.Append(ch);
+                                    else
+                                    {
+                                        started = false;
+                                        if (dataMap.ContainsKey(tmp.ToString().ToLower()))
+                                            newLine.Append(dataMap[tmp.ToString().ToLower()]);
+                                        tmp.Clear();
+                                    }
+                                }
+                                else
+                                {
+                                    if (ch == '%')
+                                        started = true;
+                                    else
+                                        newLine.Append(ch);
+                                }
+                            }
+                            writer.WriteLine(newLine.ToString());
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Process(args);
+        }
+    }
+}

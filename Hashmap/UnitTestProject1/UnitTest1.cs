fb009080f9171e05a5d2b90787038dfbc1d29c73
Aditya Iyer <aditya_iyer@yahoo.com>
2018-05-19 18:15:18 +0530

    change

--- Hashmap/UnitTestProject1/UnitTest1.cs
+++ Hashmap/UnitTestProject1/UnitTest1.cs
@@ -7,22 +7,24 @@ namespace UnitTestProject1
     [TestClass]
     public class UnitTest1
     {
+        private const int numValues = 1000000;
+
         [TestMethod]
         public void TestHashmapForInts()
         {
-            var map = new Hashmap<int, int>(1000000);
+            var map = new Hashmap<int, int>(numValues);
 
-            for (int i = 0; i < 1000000; i++)
+            for (int i = 0; i < numValues; i++)
             {
                 map.Add(i, i);
             }
 
-            for (int i = 0; i < 1000000; i++)
+            for (int i = 0; i < numValues; i++)
             {
-                Assert.AreEqual(map.Get(i), i);
+                Assert.AreEqual(i, map.Get(i));
             }
 
-            for (int i = 0; i < 1000000; i++)
+            for (int i = 0; i < numValues; i++)
             {
                 map.Remove(i);
             }
@@ -33,21 +35,21 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestHashmapForStrings()
         {
-            var map = new Hashmap<string, string>(1000000);
+            var map = new Hashmap<string, string>(numValues);
 
-            for (int i = 0; i < 1000000; i++)
+            for (int i = 0; i < numValues; i++)
             {
                 var tmp = i + string.Empty;
                 map.Add(tmp, tmp);
             }
 
-            for (int i = 0; i < 1000000; i++)
+            for (int i = 0; i < numValues; i++)
             {
                 var tmp = i + string.Empty;
-                Assert.AreEqual(map.Get(tmp), tmp);
+                Assert.AreEqual(tmp, map.Get(tmp));
             }
 
-            for (int i = 0; i < 1000000; i++)
+            for (int i = 0; i < numValues; i++)
             {
                 var tmp = i + string.Empty;
                 map.Remove(tmp);
@@ -62,19 +64,19 @@ namespace UnitTestProject1
         {
             var map = new Trie<string>();
 
-            for (int i = 0; i < 1000000; i++)
+            for (int i = 0; i < numValues; i++)
             {
                 var tmp = i + string.Empty;
                 map.Add(tmp, tmp);
             }
 
-            for (int i = 0; i < 1000000; i++)
+            for (int i = 0; i < numValues; i++)
             {
                 var tmp = i + string.Empty;
                 Assert.AreEqual(tmp, map.Get(tmp));
             }
 
-            for (int i = 0; i < 1000000; i++)
+            for (int i = 0; i < numValues; i++)
             {
                 var tmp = i + string.Empty;
                 map.Remove(tmp);
